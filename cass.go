8d574dbdc52acd7b2b6f9eac19556c10734ab931
dodevs <douglass.sousa@outlook.com.br>
2021-01-31 00:21:56 -0300

    UP: Insert function

--- cass.go
+++ cass.go
@@ -4,6 +4,15 @@ import (
 	gocql "github.com/gocql/gocql"
 )
 
+type Number struct {
+	country   int
+	ddd       int
+	number    string
+	valid     bool
+	lastView  int64
+	updatedAt int64
+}
+
 func cConnect() *gocql.Session {
 	cluster := gocql.NewCluster("127.0.0.1")
 	cluster.Keyspace = "numbersdata"
@@ -13,8 +22,10 @@ func cConnect() *gocql.Session {
 	return session
 }
 
-func cInsert(session *gocql.Session) {
-	// session.Query(
-	// 	`INSERT INTO numbers (country, ddd, number, valid, lastView, TODATE(NOW()))`
-	// )
+func cInsert(session *gocql.Session, number Number) {
+	err := session.Query(
+		`INSERT INTO numbers (country, ddd, number, valid, lastView, updatedAt) VALUES (?,?,?,?,?,toTimestamp(NOW()))`,
+		number.country, number.ddd, number.number, number.valid, number.lastView).WithContext(ctx).Exec()
+
+	errorHandler("gocql insert", err)
 }
