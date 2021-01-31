1e77b6b9cc322d5edcd339e0a4f1600a5e0d8428
Thiago Felicio Alves <thiago.alves@phidelis.com.br>
2021-01-31 17:51:20 -0300

    retirando baianagem

--- src/main.go
+++ src/main.go
@@ -11,33 +11,6 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-type Block struct {
-	Try     func()
-	Catch   func(Exception)
-	Finally func()
-}
-
-type Exception interface{}
-
-func Throw(up Exception) {
-	panic(up)
-}
-
-func (tcf Block) Do() {
-	if tcf.Finally != nil {
-
-		defer tcf.Finally()
-	}
-	if tcf.Catch != nil {
-		defer func() {
-			if r := recover(); r != nil {
-				tcf.Catch(r)
-			}
-		}()
-	}
-	tcf.Try()
-}
-
 var countrys = []int{55}
 var ddds = []int{27, 28}
 var startNumbers = []int{9, 8}
